92f6fa8911fc59472bc98f3b4274696a8cc828ce
JackieOlexa <86371072+JackieOlexa@users.noreply.github.com>
2024-03-25 12:05:20 -0600

    Update firebase.ts

--- src/util/firebase.ts
+++ src/util/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app'
 import { Firestore, doc, getDoc, getFirestore } from 'firebase/firestore/lite'
  
 const firebaseConfig = {
-  apiKey: "AIzaSyDMl26TDb3WJrEEA2tFH2Uod2-L9-uH_gI",
+  apiKey: 
   authDomain: "jackies-first-test.firebaseapp.com",
   databaseURL: "https://jackies-first-test-default-rtdb.firebaseio.com",
   projectId: "jackies-first-test",
   storageBucket: "jackies-first-test.appspot.com",
   messagingSenderId: "502972278830",
-  appId: "1:502972278830:web:607d2d455734d1c5b2d7bf"
+  appId: 
 };
 
 const app = initializeApp(firebaseConfig);
